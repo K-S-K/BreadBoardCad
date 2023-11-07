1e60c1ea1356edc85e1b1f4bf39afe2fdeb13a39
Stanislav Kiselevskii <kisilevski@gmail.com>
2023-11-08 02:30:09 +0400

    BBC-42: Add Board XML Serialization Closes #42

--- BBCAD.Itself/BoardElements/Row.cs
+++ BBCAD.Itself/BoardElements/Row.cs
@@ -7,7 +7,38 @@ namespace BBCAD.Itself.BoardElements
 {
     public class Row : ISvgElement
     {
+        #region -> Data
+        internal const string XMLRootName = "Line";
+        private const string XMLPointsName = "Points";
+        #endregion
+
+
+        #region -> Properties
         public List<Point> Points { get; set; } = new List<Point>();
+
+        internal XElement XML
+        {
+            get
+            {
+                return new XElement(XMLRootName
+                    , new XElement(XMLPointsName, Points.Select(p => p.XML)));
+            }
+            set
+            {
+                if (value.Name != XMLRootName)
+                {
+                    throw new ArgumentException(
+                        $"Element name \"{value.Name}\" instead of \"{XMLRootName}\"");
+                }
+
+                Points.Clear();
+                var items = value?
+                    .Element(XMLPointsName)?
+                    .Elements(Point.XMLRootName)
+                    .Select(xe => new Point(xe));
+                if (items != null) { Points.AddRange(items); }
+            }
+        }
 
         public IEnumerable<XElement> SVG
         {
@@ -19,6 +50,8 @@ namespace BBCAD.Itself.BoardElements
                     p => new Point((p.X + 1) * scale, (p.Y + 1) * scale))).SVG;
             }
         }
+        #endregion
+
 
         public void Add(int x, int y) => Points.Add(new Point(x, y));
 
@@ -27,5 +60,7 @@ namespace BBCAD.Itself.BoardElements
         public Row()
         {
         }
+
+        internal Row(XElement xe) : this() { XML = xe; }
     }
 }
