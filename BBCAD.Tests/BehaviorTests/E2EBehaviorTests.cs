a0874d07fd5d72d5e1bb8205c45fe8d82f612d6a
Stanislav Kiselevskii <kisilevski@gmail.com>
2023-11-18 04:41:25 +0400

    BBC-59: Implement board saving to the DB Closing #59

--- BBCAD.Tests/BehaviorTests/E2EBehaviorTests.cs
+++ BBCAD.Tests/BehaviorTests/E2EBehaviorTests.cs
@@ -4,6 +4,8 @@ using BBCAD.Cmnd;
 using BBCAD.Core;
 using BBCAD.Data;
 using BBCAD.Itself;
+using BBCAD.Cmnd.Common;
+using BBCAD.Cmnd.Commands;
 
 using Assert = NUnit.Framework.Assert;
 
@@ -44,6 +46,48 @@ namespace BBCAD.Tests.BehaviorTests
             string strActual = board.XML.ToString();
             string strExpctd = xeBoard.ToString();
             Assert.AreEqual(strExpctd, strActual);
+        }
+
+        [Test]
+        public void ExecuteComandTest()
+        {
+            IBehavior behavior = _serviceProvider.GetService<IBehavior>();
+            Assert.IsNotNull(behavior);
+
+            IScriptProcessor scriptProcessor = _serviceProvider.GetService<IScriptProcessor>();
+            Assert.IsNotNull(behavior);
+
+            IBoardStorage boardStorage = _serviceProvider.GetService<IBoardStorage>();
+            Assert.IsNotNull(boardStorage);
+
+            string script = Resources.Script_01_CRC;
+            ICommandBatch batch = scriptProcessor.ExtractCommands(script);
+            Assert.IsNotNull(batch);
+
+            var cmndCrt = batch.Commands
+                .Where(x => x.CmndType == CommandType.CreateBoard).Single() as CreateBoardCommand;
+            Assert.IsNotNull(cmndCrt);
+
+            var cmndRsz = batch.Commands
+                .Where(x => x.CmndType == CommandType.ResizeBoard).Single() as ResizeBoardCommand;
+            Assert.IsNotNull(cmndRsz);
+
+            Board board1 = behavior.ExecuteComand(cmndCrt);
+            Assert.IsNotNull(board1);
+            Assert.AreEqual(board1.Name, cmndCrt.Name.Value);
+            Assert.AreEqual(board1.SizeX, cmndCrt.X.Value);
+            Assert.AreEqual(board1.SizeY, cmndCrt.Y.Value);
+
+            // Propagate new board Id to the next command
+            XElement xeCmndRsz = cmndRsz.XML;
+            xeCmndRsz.SetAttributeValue("Id", board1.Id);
+            cmndRsz.XML = xeCmndRsz;
+
+            Board board2 = behavior.ExecuteComand(cmndRsz);
+            Assert.IsNotNull(board2);
+            Assert.AreEqual(board2.Name, cmndCrt.Name.Value);
+            Assert.AreEqual(board2.SizeX, cmndRsz.X.Value);
+            Assert.AreEqual(board2.SizeY, cmndRsz.Y.Value);
         }
 
         [Test]
